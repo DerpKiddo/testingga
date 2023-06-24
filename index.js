3f296e0d8c73be215bb30a3637559ee4596cd2ed
DerpKiddo <75645436+DerpKiddo@users.noreply.github.com>
2023-06-24 17:06:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,45 @@
-const net = require('net');
+const http = require('http');
 
 // Proxy configuration
-const PROXY_IP = '185.199.229.156';
-const PROXY_PORT = 7492;
+const PROXY_IP = '127.0.0.1';
+const PROXY_PORT = 80;
 
 // Google server details
 const GOOGLE_HOST = 'www.google.com';
-const GOOGLE_PORT = 80;
 
-function handleClient(clientSocket) {
-  // Create a connection to Google
-  const googleSocket = net.createConnection(GOOGLE_PORT, GOOGLE_HOST, () => {
-    // Forward the client request to Google
-    googleSocket.write(request);
-  });
+// Create a proxy server
+const proxy = http.createServer((req, res) => {
+  const options = {
+    host: GOOGLE_HOST,
+    port: 80,
+    path: req.url,
+    method: req.method,
+    headers: req.headers
+  };
 
-  // Receive the response from Google and send it back to the client
-  googleSocket.on('data', (data) => {
-    clientSocket.write(data);
-  });
+  // Make a request to Google using the proxy IP and address
+  const proxyReq = http.request(options, (proxyRes) => {
+    res.writeHead(proxyRes.statusCode, proxyRes.headers);
 
-  // Close the connections
-  clientSocket.on('end', () => {
-    googleSocket.end();
-  });
+    proxyRes.on('data', (chunk) => {
+      res.write(chunk);
+    });
 
-  googleSocket.on('end', () => {
-    clientSocket.end();
-  });
-}
-
-function runProxy() {
-  const proxyServer = net.createServer((clientSocket) => {
-    console.log(`Accepted connection from ${clientSocket.remoteAddress}:${clientSocket.remotePort}`);
-
-    // Receive client request
-    clientSocket.on('data', (data) => {
-      const request = data;
-
-      // Create a new thread to handle the client request
-      handleClient(clientSocket, request);
+    proxyRes.on('end', () => {
+      res.end();
     });
   });
 
-  proxyServer.listen(PROXY_PORT, PROXY_IP, () => {
-    console.log(`Proxy server running on ${PROXY_IP}:${PROXY_PORT}`);
+  req.on('data', (chunk) => {
+    proxyReq.write(chunk);
   });
-}
 
-runProxy();
+  req.on('end', () => {
+    proxyReq.end();
+  });
+});
+
+// Start the proxy server
+proxy.listen(PROXY_PORT, PROXY_IP, () => {
+  console.log(`Psadsaroxy server running on ${PROXY_IP}:${PROXY_PORT}`);
+});
